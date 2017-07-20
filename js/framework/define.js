56b72c634fcf445b84bfd972c2d297fdf0a5e31c
lanfeng <23229437@qq.com>
2017-07-20 09:56:36 +0800

    工程搭建

---
+++ js/framework/define.js
@@ -0,0 +1,27 @@
+/**
+ * Created by lanfeng on 2016/5/10.
+ */
+
+
+//开发服务器路径
+var SERVER_BAS_URL = "http://120.26.37.73:8091/shoot-mobile-web";//开发
+
+var REQUESTDATA = {
+    "sysType": "1",
+    "appKey": "123456",
+    "timestamp":"",
+    "tokenId":"",
+    "sign": "",
+    "data": {}
+};
+
+var DEFAULTSIGNATUREPWD = "";
+
+/*
+ * 日志级别配置
+ * OFF、ERROR、WARN、INFO、DEBUG、ALL
+ */
+var debug = "ALL";
+
+
+
