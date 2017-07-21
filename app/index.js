11fb0808bdee8e2d39a478f017b61f8d7732b09a
xiongyan <1103252680@qq.com>
2017-07-21 16:30:29 +0800

    首页修改

---
+++ app/index.js
@@ -0,0 +1,19 @@
+var vm = new Vue({
+	el: '#app',
+	data: {
+		pendingSum: 0,
+		completedSum: 0,
+		cancelSum: 0,
+		assignmentSum: 0
+	}
+})
+lf.ready(function() {
+	/*var a = lf.window.currentWebview().a
+	var b =lf.window.currentWebview().b
+	console.log(a)
+	console.log(b)
+	lf.event.fire(lf.window.currentWebview().opener(),'test',{
+		a:1,b:2
+	})*/
+
+})
