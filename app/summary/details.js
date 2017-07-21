73881c8acd4304a33f7c2a691b82ab10c99c40e5
liu shuai <550271002@qq.com>
2017-07-21 11:44:56 +0800

    '录入心得'

--- app/summary/details.js
+++ app/summary/details.js
@@ -1,16 +1,51 @@
-var vm = null
-
 lf.ready(function () {
-    var mask = mui.createMask();
-    vm = new Vue({
+    var pageParams = {
+        orderId: '',
+        photographerId: ''
+    }
+    function setPageParams(params) {
+        mui.each(pageParams,function (key) {
+            if(key in params){
+                pageParams[key] = params[key]||''
+            }
+        })
+    }
+    mui.plusReady(function(){
+        var currentWebview = plus.webview.currentWebview();
+        setPageParams(currentWebview)
+    });
+    window.addEventListener('customEvent',function(event){
+        setPageParams(event)
+    });
+
+    var vm = new Vue({
         el: '#app',
         data: function () {
             return {
                 // 代表作品 图片数组
-                imgs: [],
+                imgs: ['http://img.fingercrm.cn/9fc9255b0875470cb6854aee0a4dcc14.jpg','http://img.fingercrm.cn/9fc9255b0875470cb6854aee0a4dcc14.jpg'],
                 // 拍摄总结  string
-                summary: ''
+                summary: '非常好非常好非常好非常好非常好非常好非常好非常好非常好非常好非常好非常好非常好非常好非常好非常好'
             }
+        },
+        created: function () {
+            var self = this
+            lf.nativeUI.showWaiting()
+            lf.net.getJSON('/order/getOrderPhotographer', {
+                orderId: pageParams.orderId,
+                photographerId: pageParams.photographerId
+            }, function (res) {
+                lf.nativeUI.closeWaiting()
+                if (res.code === '200') {
+                    self.imgs = res.data.imgs || []
+                    self.summary = res.data.summary || ''
+                } else {
+                    mui.toast(res.msg)
+                }
+            }, function () {
+                lf.nativeUI.closeWaiting()
+                mui.toast(res.msg)
+            })
         },
         mounted: function () {
             this.$nextTick(function () {
