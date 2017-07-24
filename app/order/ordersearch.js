657467b22ee0c2fdc0264fca9beedd1de48d7dda
lanfeng <23229437@qq.com>
2017-07-24 17:00:02 +0800

    搜索

--- app/order/ordersearch.js
+++ app/order/ordersearch.js
@@ -8,38 +8,78 @@ var vm = new Vue({
 		pageNos:[
 
 		],
+		his:[],
 		pageNum: 10
 	}
 })
+var HOT_SEARCH = 'HOT_SEARCH'
 lf.ready(function() {
 	initPull();
+	
+	//搜索记录
+	var jl = lf.storage.get(HOT_SEARCH)
+	console.log(jl)
+	if(typeof jl != 'undefined'&& jl != null){
+		vm.his = JSON.parse(jl)
+	}
 	//点击搜索按钮
 	document.getElementById('searchBtn').addEventListener('tap',function(){
-		vm.showAnswer = true;
-		vm.pageNos[0]=1;
-		var params = {
-			searchText:vm.searchTest,
-			status:'',
-			currPage :vm.pageNos[0],
-			pageSize : vm.pageNum
-
-		}
-		lf.nativeUI.showWaiting();
-		lf.net.getJSON('/order/search',params,function(data){
-				lf.nativeUI.closeWaiting();
-				if(data.code == "200"){
-				vm.orderList = data.data.result;
-				console.log("orderList=="+JSON.stringify(data))
-			}else{
-				lf.nativeUI.toast(data.msg);
-			}
-		},function(error){
-			lf.nativeUI.closeWaiting();
-			lf.nativeUI.toast(error.msg);
-		})
+		findData();
 	})
 })
 
+document.getElementById('clearBtn').addEventListener('tap',function(){
+	lf.storage.removeItem(HOT_SEARCH)
+	vm.his = [];
+})
+
+mui('.mui-content').on('tap','.hisbtn',function(){
+	console.log(this.innerText)
+	vm.searchText = this.innerText;
+	findData();
+})
+function findData(){
+	vm.pageNos[0]=1;
+	
+	//处理搜索
+	if(vm.searchText){
+		var flag = true;
+		for(var i in vm.his){
+			if(vm.his[i] == vm.searchText){
+				flag = false;
+				break;
+			}
+		}
+		if(flag){
+			vm.his.push(vm.searchText)
+			lf.storage.put(HOT_SEARCH,JSON.stringify(vm.his))
+		}
+	}
+	var params = {
+		searchText:vm.searchText,
+		status:'',
+		currPage :vm.pageNos[0],
+		pageSize : vm.pageNum
+
+	}
+	lf.nativeUI.showWaiting();
+	lf.net.getJSON('/order/search',params,function(data){
+		lf.nativeUI.closeWaiting();
+		if(data.code == "200"){
+			if(data.data.result.length > 0){
+				vm.showAnswer = true;
+				vm.orderList = data.data.result;
+			}else{
+				lf.nativeUI.toast('查询不到数据');
+			}
+		}else{
+			lf.nativeUI.toast(data.msg);
+		}
+	},function(error){
+		lf.nativeUI.closeWaiting();
+		lf.nativeUI.toast(error.msg);
+	})
+}
 function dodata(type, index, data) {
 	if(type == 'up') {
 		vm.orderList = vm.orderList.concat(data)
@@ -65,7 +105,7 @@ function initPull() {
 						var self = this;
 						vm.pageNos[index] = 1;
 						var params = {
-							searchText:vm.searchTest,
+							searchText:vm.searchText,
 							status:'',
 							currPage:vm.pageNos[index],
 							pageSize:vm.pageNum
@@ -87,12 +127,11 @@ function initPull() {
 					}
 				},
 				up: {
-					auto: true,
 					callback: function() {
 						var self = this;
 						vm.pageNos[index]++;
 						var params = {
-							searchText:vm.searchTest,
+							searchText:vm.searchText,
 							status:'',
 							currPage:vm.pageNos[index],
 							pageSize:vm.pageNum
