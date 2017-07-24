0281cd4290e5e5b3396a0cfcc0a8d97be9d3e825
陈炜婧 <375237566@qq.com>
2017-07-24 10:54:02 +0800

    搜索订单html修改

--- app/order/ordersearch.js
+++ app/order/ordersearch.js
@@ -3,44 +3,61 @@ var vm = new Vue({
 	data: {
 		searchText: '',
 		showAnswer:false,//是否显示搜索结果
-		orderList: [{
-				orderNo: 'dd1',//订单编号
-				status: '5',//订单状态
-				tourGuide: '导游1号',//导游姓名
-				tourNo: '1',//团号
-				productName产品: '桂林山水文化',
-				purchaser: '采购方1',//采购方
-				totalPrice: 1000 //订单金额
-			},
-			{
-				orderNo: 'dd2',//订单编号
-				status: '5',//订单状态
-				tourGuide: '导游2号',//导游姓名
-				tourNo: '2',//团号
-				productName产品: '桂林山水文化',
-				purchaser: '采购方2',//采购方
-				totalPrice: 2000 //订单金额
-			},
-			{
-				orderNo: 'dd3',//订单编号
-				status: '5',//订单状态
-				tourGuide: '导游3号',//导游姓名
-				tourNo: '3',//团号
-				productName产品: '桂林山水文化',
-				purchaser: '采购方3',//采购方
-				totalPrice: 3000 //订单金额
-			}
-		]
+		seachTest :'',//搜索关键字
+//		orderList: [{
+//				orderNo: 'dd1',//订单编号
+//				status: '5',//订单状态
+//				tourGuide: '导游1号',//导游姓名
+//				tourNo: '1',//团号
+//				productName产品: '桂林山水文化',
+//				purchaser: '采购方1',//采购方
+//				totalPrice: 1000 //订单金额
+//			},
+//			{
+//				orderNo: 'dd2',//订单编号
+//				status: '5',//订单状态
+//				tourGuide: '导游2号',//导游姓名
+//				tourNo: '2',//团号
+//				productName产品: '桂林山水文化',
+//				purchaser: '采购方2',//采购方
+//				totalPrice: 2000 //订单金额
+//			},
+//			{
+//				orderNo: 'dd3',//订单编号
+//				status: '5',//订单状态
+//				tourGuide: '导游3号',//导游姓名
+//				tourNo: '3',//团号
+//				productName产品: '桂林山水文化',
+//				purchaser: '采购方3',//采购方
+//				totalPrice: 3000 //订单金额
+//			}
+//		]
+		orderList: []
 	}
 })
 lf.ready(function() {
-//	mui('.searchicon').on('tap',function(){
-//		console.log(111)
-//		vm.showAnswer = true;
-//	})
+
+	//点击搜索按钮
 	document.getElementById('searchBtn').addEventListener('tap',function(){
 		console.log('search:'+ vm.searchText)
 		vm.showAnswer = true;
+		var params = {
+			seachTest :vm.seachTest,
+			currPage :1,
+			pageSize : 10
+
+		}
+		console.log("params="+JSON.stringify(params))
+		lf.net.getJSON("/order/search",params,function(data){
+			console.log("data="+JSON.stringify(data))
+			if(data.code == "200"){
+			vm.orderList = data.result;
+			}else{
+				mui.alert(data.msg)
+			}
+		},function(error){
+			mui.alert(error.msg, '错误');
+		})
 	})
 })
 //
