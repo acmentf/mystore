d4e98a7e291356da8d1754e935a82bc782e15e2c
xiongyan <1103252680@qq.com>
2017-07-25 09:55:57 +0800

    文案修改

--- app/index.js
+++ app/index.js
@@ -48,7 +48,7 @@ mui('.toolbar').on('tap', '.message', function() {
 })
 
 mui('.toolbar').on('tap', '.icon-tuichu', function() {
-	lf.nativeUI.confirm("操作提示", "确定要能出当前用户吗?", ["确定", "取消"], function(e) {
+	lf.nativeUI.confirm("操作提示", "确定要退出当前用户吗?", ["确定", "取消"], function(e) {
 		if(e.index == 0) {
 			window.Role.logout();
 			plus.runtime.restart();
