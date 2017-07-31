0120c84de9eb70c9f42f5e2ab3ec8b9b3f5baaec
lk <443283633@qq.com>
2017-07-31 21:35:53 +0800

    销售输出

--- app/result/order-result.js
+++ app/result/order-result.js
@@ -1,39 +1,50 @@
 var vm = new Vue({
 	el: '#app',
 	data: {
-		cut:1,
-		orderId: '',
+		saleStatus:1,
+		orderId: '33',
 		id: '',
-		shootPeoNum: '',
-		shootNum: '',
-		selectsNum: '',
-		printsData: [
+		printOrderXms: [
 		{
-			printsNum:'',
-			printsSize:'',
-			printsSizeName:''
+			fType: '',
+			id: '',
+			orderId: '',
+			picNum: '',
+			picSize: '',
+			picSizeName: '',
+			price: ''
 		}
 		],
-		givesData: [
-		{
-			givesNum:'',
-			givesSize:''
+		giveOrderXms: [{
+			fType: '',
+			id: '',
+			orderId: '',
+			picNum: '',
+			picSize: '',
+			picSizeName: '',
+			price: ''
 		}],
-		salesData: [
-		{
-			salesNum:'',
-			salesSize:''
+		saleOrderXms: [{
+			fType: '',
+			id: '',
+			orderId: '',
+			picNum: '',
+			picSize: '',
+			picSizeName: '',
+			price: ''
 		}],
 		salesAmt: '',
 		buyers: '',
-		receiveNum: '',
+		remark: '',
 		saleDate: '',
+		reason:''
 	},
 	methods: {
 		
 	}
 })
 lf.ready(function(){
+	
 	var opts = {"type": "date"};
 	picker = new mui.DtPicker(opts);
 	userPicker = new mui.PopPicker();
@@ -53,60 +64,191 @@ lf.ready(function(){
 		value: '5',
 		text: '6寸'
 	}]);
+	reasonPicker = new mui.PopPicker();
+	reasonPicker.setData(['景点禁售','其他']);
+	loadResult()
+	var wv = lf.window.currentWebview()
+	vm.orderId = wv.orderNo	
+	
 })
+//尺寸选择器S
 mui('.mui-content').on('tap', '.printsSize', function() {
 	var index = this.getAttribute('data-index');
 	userPicker.show(function(items) {
-		Vue.set(vm.printsData,index,{
-			printsNum: vm.printsData[index].printsNum,
-			printsSize: items[0].value,
-			printsSizeName: items[0].text
+		Vue.set(vm.printOrderXms,index,{
+			picNum: vm.printOrderXms[index].picNum,
+			picSize: items[0].value,
+			picSizeName: items[0].text
 		})
 	});
 })
 mui('.mui-content').on('tap', '.givesSize', function() {
+	var index = this.getAttribute('data-index');
 	userPicker.show(function(items) {
-		vm.givesData.forEach(function(v){
-			v.givesSize= items[0].text
-		}) 
+		Vue.set(vm.giveOrderXms,index,{
+			picNum: vm.giveOrderXms[index].givesNum,
+			picSize: items[0].value,
+			picSizeName: items[0].text
+		})
 	});
 })
 mui('.mui-content').on('tap', '.salesSize', function() {
+	var index = this.getAttribute('data-index');
 	userPicker.show(function(items) {
-		vm.salesData.forEach(function(v){
-			v.salesSize= items[0].text
-		}) 
+		Vue.set(vm.saleOrderXms,index,{
+			picNum: vm.saleOrderXms[index].salesNum,
+			picSize: items[0].value,
+			picSizeName: items[0].text
+		})
 	});
 })
+//时间选择器
 mui('.mui-content').on('tap', '.selectDate', function(){
 	picker.show(function(items) {
 		vm.saleDate = items.value
 	});
 })
+//原因选择
+mui('.mui-content').on('tap','.select-reason',function(){
+	reasonPicker.show(function(items) {
+		vm.reason = items[0]
+	})
+})
+//尺寸选择器E
 mui('.mui-content').on('tap', '.add-printsNum', function() { //添加打印张数
-	vm.printsData.push({
-			printsNum:'',
-			printsSize:''
-		})
+	vm.printOrderXms.push({fType: '',
+			id: '',
+			orderId: '',
+			picNum: '',
+			picSize: '',
+			picSizeName: '',
+			price: ''})
 })
 mui('.mui-content').on('tap', '.add-givesNum', function() { //添加赠送张数
-	vm.givesData.push({
-			givesNum:'',
-			givesSize:''
-		})
+	vm.giveOrderXms.push({fType: '',
+			id: '',
+			orderId: '',
+			picNum: '',
+			picSize: '',
+			picSizeName: '',
+			price: ''})
 })
 mui('.mui-content').on('tap', '.add-salesNum', function() { //添加销售张数
-	vm.salesData.push({
-			salesNum:'',
-			salesSize:''
-		})
+	vm.saleOrderXms.push({fType: '',
+			id: '',
+			orderId: '',
+			picNum: '',
+			picSize: '',
+			picSizeName: '',
+			price: ''})
 })
-mui('.mui-content').on('tap', '.remove-icon', function(){
+//移除S
+mui('.mui-content').on('tap', '.remove-printsNum', function(){
 	var index = this.getAttribute('data-index');
-	console.log(index , JSON.stringify(vm.printsData) )
-	vm.printsData.splice(index,1)
+	vm.printOrderXms.splice(index,1)
 })
+mui('.mui-content').on('tap', '.remove-salesNum', function(){
+	var index = this.getAttribute('data-index');
+	vm.saleOrderXms.splice(index,1)
+})
+mui('.mui-content').on('tap', '.remove-givesNum', function(){
+	var index = this.getAttribute('data-index');
+	vm.giveOrderXms.splice(index,1)
 
-mui('.mui-content').on('tap', '#save', function(){
-	console.log(JSON.stringify(vm.printsData))
 })
+//移除E
+mui('.mui-content').on('tap', '#save', function(){
+	var params1 = {
+		orderId:vm.orderID,
+		saleStatus:vm.saleStatus,
+		saleDate:vm.saleDate,
+		printOrderXms:vm.printOrderXms,
+		saleOrderXms:vm.saleOrderXms,
+		giveOrderXms:vm.giveOrderXms,
+		buyers:vm.buyers,
+		salesAmt:vm.salesAmt,
+		remark:vm.remark
+	}
+	var params2 = {
+		orderId:vm.orderId,
+		saleStatus:vm.saleStatus,
+		reason:vm.reason,
+		remark:vm.remark
+
+	}
+	var params
+	if(vm.saleStatus==1){
+		params = params1
+		var reg= /[^\d]/g;
+		flag = true
+		params.printOrderXms.forEach(function(v){
+		if(reg.test(v.picNum)){
+			lf.nativeUI.toast('请输入数字')
+			flag = false
+		}
+		})
+		params.saleOrderXms.forEach(function(v){
+		if(reg.test(v.picNum)){
+			lf.nativeUI.toast('请输入数字')
+			flag = false
+		}
+		})
+		params.giveOrderXms.forEach(function(v){
+		if(reg.test(v.picNum)){
+			lf.nativeUI.toast('请输入数字')
+			flag = false
+		}
+		})
+	}else{
+		params = params2
+		if(vm.reason == '其他'&&vm.remark==""){
+			lf.nativeUI.toast('请在备注中填写原因');
+			return
+		}
+	}
+	if(flag){
+		lf.nativeUI.showWaiting()
+		lf.net.getJSON('order/saveOrderSaleResult', params, function(res) {
+		lf.nativeUI.closeWaiting()
+		if(res.code == 200) {
+			lf.nativeUI.toast('保存成功！');
+			lf.event.fire(lf.window.currentWebview().opener(), 'orderdetails', {})
+			lf.window.closeCurrentWebview();
+		} else {
+			lf.nativeUI.toast(res.msg);
+		}
+	}, function(res) {
+		lf.nativeUI.closeWaiting()
+		lf.nativeUI.toast(res.msg)
+	})
+	}
+})
+function loadResult(){
+	var params={
+		orderId:vm.orderId
+	}
+	lf.net.getJSON('/order/queryOrderSaleResult', params, function (res){
+		if(res.code == 200){
+			if(res.data == null){
+				return 
+			}else{
+				vm.saleOrderXms = res.data.saleOrderXms || []
+				vm.giveOrderXms = res.data.giveOrderXms || []
+				vm.saleOrderXms = res.data.saleOrderXms || []
+				vm.buyers = res.data.buyers
+				vm.remark = res.data.remark
+				vm.saleStatus = res.data.saleStatus
+				vm.salesAmt = res.data.salesAmt
+			}
+		}else {
+			lf.nativeUI.toast(res.msg);
+		}
+	}, function(res) {
+		lf.nativeUI.closeWaiting()
+		lf.nativeUI.toast(res.msg)
+	})
+}
+
+
+
+
