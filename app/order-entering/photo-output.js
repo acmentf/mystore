a4b47f8489b3414a3fd1fbb61924d962316a01bf
陈炜婧 <375237566@qq.com>
2017-07-31 17:30:27 +0800

    拍摄输出渲染

--- app/order-entering/photo-output.js
+++ app/order-entering/photo-output.js
@@ -2,17 +2,32 @@ var vm = new Vue({
 	el: '#app',
 	data: {
 		orderId: '',
-		photoOutList: []
+		photoOutList: [],
+		remark:''
 	}
 })
-
 lf.ready(function(){
 	vm.orderId = lf.window.currentWebview().orderNo;
 	init();
 })
+mui('.mui-content').on('tap','#saveBtn',function(){
+	console.log('save')
+	return;
+	var params = {
+		"orderId":vm.orderId
+	};
+	lf.nativeUI.showWaiting()
+	lf.net.getJSON('/order/saveOrderShootResult',params,function (res) {
+		lf.nativeUI.closeWaiting()
+		if(res.code == 200) {
 
-document.getElementById('save').addEventListener('tap',function(){
-
+		}else{
+			lf.nativeUI.toast(res.msg)
+		}
+    },function(res){
+    	lf.nativeUI.closeWaiting()
+    	lf.nativeUI.toast(res.msg)
+    })
 })
 
 // 请求数据
@@ -25,7 +40,7 @@ function init(){
 		lf.nativeUI.closeWaiting()
 		if(res.code == 200) {
 			lf.nativeUI.toast('操作成功')
-
+			console.log(res)
 		}else{
 			lf.nativeUI.toast(res.msg)
 		}
