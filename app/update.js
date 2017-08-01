8f5f2e23b445eb22984071304d0813b44d8cb378
lanfeng <23229437@qq.com>
2017-08-01 17:13:29 +0800

    更新模块

--- app/update.js
+++ app/update.js
@@ -9,34 +9,32 @@ function update() {
 		"imei": plus.device.imei,
 		"platform": plus.os.name
 	};
-	lf.net.getJSON("/version", params, function(data) {
+	lf.net.getJSON("/app/validationversion", params, function(data) {
 		var update_desc = "发现新的版本，是否需要立即更新";
-		if(data.result_code == 0) {
-			if(data.result_data.no_version == 0) {
-				var btns = null;
-				if(data.result_data.upgrade_type == 2) {
-					update_desc = "发现新的版本，请立即更新";
-					btns = ["立即更新"];
-				} else {
-					btns = ["立即更新", "取　　消"];
-				}
-				if(data.result_data.upgrade_desc) {
-					update_desc = update_desc + "\n" + data.result_data.upgrade_desc;
-				}
-				lf.nativeUI.confirm("", update_desc, btns, function(e) {
-					if(btns.length == 1) {
-						if(0 == e.index) {
-							plus.runtime.openURL(data.result_data.upgrade_url);
-						} else {
-							plus.runtime.quit();
-						}
-					} else {
-						if(0 == e.index) {
-							plus.runtime.openURL(data.result_data.upgrade_url);
-						} else {}
-					}
-				});
+		if(data.code == 200) {
+			var btns = null;
+			if(data.data.isMandatory == 1) {
+				update_desc = "发现新的版本，请立即更新";
+				btns = ["立即更新"];
+			} else {
+				btns = ["立即更新", "取　　消"];
 			}
-		} else {}
+			if(data.data.upgrade_desc) {
+				update_desc = update_desc + "\n" + data.data.releaseRemark;
+			}
+			lf.nativeUI.confirm("", update_desc, btns, function(e) {
+				if(btns.length == 1) {
+					if(0 == e.index) {
+						plus.runtime.openURL(data.data.releaseUrl);
+					} else {
+						plus.runtime.quit();
+					}
+				} else {
+					if(0 == e.index) {
+						plus.runtime.openURL(data.data.releaseUrl);
+					} else {}
+				}
+			});
+		}
 	}, function(res) {});
 }
