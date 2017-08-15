043eeeda5451778f514ba8c58629034c2f7819d5
xiongyan <1103252680@qq.com>
2017-08-15 17:39:58 +0800

    订单状态日志

--- app/order/statuslog.js
+++ app/order/statuslog.js
@@ -1,56 +1,89 @@
 var vm = new Vue({
-	el: '#trackinfo',
+	el: '#app',
 	data: {
-		"0": [{
-				"createTime": 1502380800000,
-				"creator": 1,
-				"id": 1,
-				"logSource": 1,
-				"orderId": 1,
-				"orderNo": "1",
-				"orderPreStatus": 0,
-				"orderStatus": 0,
-				"remark": "未完成"
-			},
-			{
-				"createTime": 1502380800000,
-				"creator": 1,
-				"id": 2,
-				"logSource": 1,
-				"orderId": 1,
-				"orderNo": "1",
-				"orderPreStatus": 0,
-				"orderStatus": 0,
-				"remark": "未完成1"
-			}
-		],
-		"1": [{
-				"createTime": 1502380800000,
-				"creator": 1,
-				"id": 3,
-				"logSource": 1,
-				"orderId": 1,
-				"orderNo": "1",
-				"orderPreStatus": 0,
-				"orderStatus": 1,
-				"remark": "已完成计调1"
-			},
-			{
-				"createTime": 1502380800000,
-				"creator": 1,
-				"id": 4,
-				"logSource": 1,
-				"orderId": 1,
-				"orderNo": "1",
-				"orderPreStatus": 1,
-				"orderStatus": 1,
-				"remark": "已完成计调2"
-			}
-		]
+		currentOrderId: '',
+		statusData0: [],
+		statusData1: [],
+		statusData2: [],
+		statusData3: [],
+		statusData4: [],
+		statusData0Show: false,
+		statusData1Show: false,
+		statusData2Show: false,
+		statusData3Show: false,
+		statusData4Show: false,
+
 	}
 })
-var picker = null;
+
 lf.ready(function() {
-	vm.assignRole = window.Role.hasAuth('assign'); // 指派按钮的key
+	vm.currentOrderId = lf.window.currentWebview().orderid
+	console.log("接收到的id........." + vm.currentOrderId)
+	getStatusLogList()
 
 })
+
+function getStatusLogList() {
+
+	var params = {
+		orderId: vm.currentOrderId,
+	};
+	lf.net.getJSON('order/queryOrderLogs', params, function(data) {
+		if(data.code == 200) {
+			if(data.data != '') {
+				console.log("这也能跳进来？")
+				vm.statusData0 = data.data.s0
+				vm.statusData1 = data.data.s1
+				vm.statusData2 = data.data.s2
+				vm.statusData3 = data.data.s3
+				vm.statusData4 = data.data.s4
+				if(vm.statusData0) {
+					if(vm.statusData0.length > 0) {
+						vm.statusData0Show = true
+						vm.statusData0.forEach(function(v, i) {
+							v.createTime = lf.util.timeStampToDate(v.createTime)
+						})
+					}
+				}
+				if(vm.statusData1) {
+					if(vm.statusData1.length > 0) {
+						vm.statusData1Show = true
+						vm.statusData1.forEach(function(v, i) {
+							v.createTime = lf.util.timeStampToDate(v.createTime)
+						})
+					}
+				}
+				if(vm.statusData2) {
+					if(vm.statusData2.length > 0) {
+						vm.statusData2Show = true
+						vm.statusData2.forEach(function(v, i) {
+							v.createTime = lf.util.timeStampToDate(v.createTime)
+						})
+					}
+				}
+				if(vm.statusData3) {
+					if(vm.statusData3.length > 0) {
+						vm.statusData3Show = true
+						vm.statusData3.forEach(function(v, i) {
+							v.createTime = lf.util.timeStampToDate(v.createTime)
+						})
+					}
+				}
+				if(vm.statusData4) {
+					if(vm.statusData4.length > 0) {
+						vm.statusData4Show = true
+						vm.statusData4.forEach(function(v, i) {
+							v.createTime = lf.util.timeStampToDate(v.createTime)
+						})
+					}
+				}
+
+			}
+
+		} else {
+			lf.nativeUI.toast(data.msg);
+		}
+	}, function(erro) {
+		lf.nativeUI.toast(erro.msg);
+	});
+}
