3769b0574b15f6be541544bf09c98c4572f749b0
LY-20170805HEGJ\Administrator <443283633@qq.com>
2017-08-26 16:26:38 +0800

    111

--- app/order/orderlist.js
+++ app/order/orderlist.js
@@ -98,7 +98,7 @@ lf.ready(function() {
 		default:
 			break;
 	}
-
+	update() 
 })
 /*document.getElementById('searchDiv').addEventListener('tap',function(){
 	lf.window.openWindow('ordersearch.html', 'ordersearch.html')
@@ -547,6 +547,43 @@ function find(index) {
 		vm.pageNos[index]--;
 		lf.nativeUI.toast(res.msg)
 	})
+}
+function update() {
+	var params = {
+		"app_id": plus.runtime.appid,
+		"version": plus.runtime.version,
+		"imei": plus.device.imei,
+		"platform": plus.os.name
+	};
+	lf.net.getJSON("/app/validationversion", params, function(data) {
+		var update_desc = "发现新的版本，是否需要立即更新";
+		if(data.code == 200) {
+			var btns = null;
+			console.log(data.data.releaseUrl)
+			if(data.data.isMandatory == 1) {
+				update_desc = "发现新的版本，请立即更新";
+				btns = ["立即更新"];
+			} else {
+				btns = ["立即更新", "取　　消"];
+			}
+			if(data.data.upgrade_desc) {
+				update_desc = update_desc + "\n" + data.data.releaseRemark;
+			}
+			lf.nativeUI.confirm("", update_desc, btns, function(e) {
+				if(btns.length == 1) {
+					if(0 == e.index) {
+						plus.runtime.openURL(data.data.releaseUrl);
+					} else {
+						plus.runtime.quit();
+					}
+				} else {
+					if(0 == e.index) {
+						plus.runtime.openURL(data.data.releaseUrl);
+					} else {}
+				}
+			});
+		}
+	}, function(res) {});
 }
 lf.event.listener('orderdetails', function(e) {
 	vm.orderList.forEach(function(v, i) { // 将数据制空
