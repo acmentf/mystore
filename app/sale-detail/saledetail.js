f1a71f861b22c8c28c2b4b78080f2fd5f7ce6dfd
liziping <liziping@fingercrm.cn>
2017-09-12 13:20:52 +0800

    update sale

--- app/sale-detail/saledetail.js
+++ app/sale-detail/saledetail.js
@@ -12,12 +12,6 @@ lf.ready(function() {
     initPull();
 })
 function initPull() {
-	var deceleration = mui.os.ios ? 0.003 : 0.0009;
-	mui('.mui-scroll-wrapper').scroll({
-		bounce: false,
-		indicators: true, 
-		deceleration: deceleration
-	});
 	mui.ready(function() {
         mui(".content").pullToRefresh({
             up : {
@@ -29,6 +23,7 @@ function initPull() {
                         curPage: vm.curPage
                     };
                     console.log(orderId)
+                    var _this = this;
                     lf.net.getJSON('pay/getOrderList', params, function(res) {
                         console.log(JSON.stringify(res))
                         if (res.code == 200) {
@@ -38,6 +33,9 @@ function initPull() {
                                 vm.totalSalePersonnelNum = res.data[0].totalSalePersonnelNum || '0'
                                 vm.totalSaleNum = res.data[0].totalSaleNum || '0'
                                 vm.curPage = vm.curPage + 1
+                                _this.endPullUpToRefresh(false)
+                            } else {
+                                _this.endPullUpToRefresh(true)
                             }
                         } else {
                             lf.nativeUI.toast(data.msg);
