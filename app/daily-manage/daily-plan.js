b2e7d237ceb2c277ed64248f8c14329a14da1d89
liziping <liziping@fingercrm.cn>
2017-09-27 14:15:07 +0800

    daily plan add verification

--- app/daily-manage/daily-plan.js
+++ app/daily-manage/daily-plan.js
@@ -14,16 +14,34 @@ lf.ready(function() {
     // 保存计划
     mui("body").on("tap", ".save-btn", function() {
         for(var i=0;i<vm.planList.length;i++) {
+            var reg = /^[1-9]\d*$/
+            var amountReg = /^[0-9]+([.]{1}[0-9]{1,2})?$/
+            if(vm.planList[i].planPersons&&!reg.test(vm.planList[i].planPersons)){
+                lf.nativeUI.toast('请输入正确的预发团人数');
+                return false
+            }
+            if(vm.planList[i].planAmount&&vm.planList[i].planAmount<0.01){
+                lf.nativeUI.toast('预销售额不能小于0.01')
+                return false
+            }
+            if(vm.planList[i].planAmount&&!amountReg.test(vm.planList[i].planAmount)){
+                lf.nativeUI.toast('预销售额只能精确到两位小数');
+                return false
+            }
+            if(vm.planList[i].planShootNums&&!reg.test(vm.planList[i].planShootNums)){
+                lf.nativeUI.toast('请输入正确的预拍人数');
+                return false
+            }
             if(!vm.planList[i].planPersons&&(vm.planList[i].planShootNums||vm.planList[i].planAmount)) {
-                lf.nativeUI.toast('请输入发团人数');
+                lf.nativeUI.toast('请输入预发团人数');
                 return false
             }
             if((vm.planList[i].planPersons||vm.planList[i].planShootNums)&&!vm.planList[i].planAmount) {
-                lf.nativeUI.toast('请输入金额');
+                lf.nativeUI.toast('请输入预销售额');
                 return false
             }
             if((vm.planList[i].planPersons||vm.planList[i].planAmount)&&!vm.planList[i].planShootNums) {
-                lf.nativeUI.toast('请输入拍摄人数');
+                lf.nativeUI.toast('请输入预拍人数');
                 return false
             }
         }
