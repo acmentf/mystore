266f4b9ac462631df4d4a681a974b3c940a8bb8b
liu shuai <550271002@qq.com>
2017-10-30 10:41:35 +0800

    a

--- app/statistics/daily-paper.js
+++ app/statistics/daily-paper.js
@@ -1083,48 +1083,42 @@
                     cb && cb()
                     lf.nativeUI.toast(error.msg);
                 });
-            }
+            },
             /*历史模块 end*/
+            init: function () {
+                var self = this
+                this.initRegionProvince(init)
+                setTimeout(function () {
+                    self.intervalMap.timeout = setInterval(function () {
+                        var startIndex = self.intervalMap.startIndex
+                        switch (self.pageTypeActive){
+                            case pageTypeConstant.income:
+                                startIndex.incomeLine += 1
+                                startIndex.incomeTravel += 1
+                                startIndex.incomeAll += 1
+                                startIndex.incomeRegion += 1
+                                break
+                            case pageTypeConstant.flow:
+                                startIndex.flowShootLine += 1
+                                startIndex.flowShootTravel += 1
+                                startIndex.flowShootAll += 1
+                                startIndex.flowShootRegion += 1
+                                break
+                            case pageTypeConstant.history:
+                                startIndex.historyIncomeDay += 1
+                                startIndex.historyShootDay += 1
+                                break
+                        }
+                    }, 1000);
+                }, 300)
+            }
         },
         created: function () {
-            var self = this
-            this.initRegionProvince(init)
-            setTimeout(function () {
-                self.intervalMap.timeout = setInterval(function () {
-                    var startIndex = self.intervalMap.startIndex
-                    switch (self.pageTypeActive){
-                        case pageTypeConstant.income:
-                            startIndex.incomeLine += 1
-                            startIndex.incomeTravel += 1
-                            startIndex.incomeAll += 1
-                            startIndex.incomeRegion += 1
-                            break
-                        case pageTypeConstant.flow:
-                            startIndex.flowShootLine += 1
-                            startIndex.flowShootTravel += 1
-                            startIndex.flowShootAll += 1
-                            startIndex.flowShootRegion += 1
-                            break
-                        case pageTypeConstant.history:
-                            startIndex.historyIncomeDay += 1
-                            startIndex.historyShootDay += 1
-                            break
-                    }
-                }, 1000);
-            }, 300)
         },
         beforeDestroy: function () {
             clearInterval(self.intervalMap.timeout)
         }
     })
-    if (window.Role.currentPositions.length > 0) {
-        vm.currentRoleId = window.Role.currentPositions[0].roleId;
-        console.log("当前用户的角色id" + vm.currentRoleId)
-    }
-    vm.rolePositionId = window.Role.userroleId // 岗位id
-    vm.username = window.Role.username // 用户昵称
-    vm.rolePositionList = window.Role.positions // 岗位列表
-    console.log(JSON.stringify(vm.rolePositionList))
 
     // 获取大区省份
     function getRegionProvince(options, text) {
@@ -1185,6 +1179,17 @@
         }
     }
     lf.ready(function() {
+        if (window.Role.currentPositions.length > 0) {
+            vm.currentRoleId = window.Role.currentPositions[0].roleId;
+            console.log("当前用户的角色id" + vm.currentRoleId)
+        }
+        vm.rolePositionId = window.Role.userroleId // 岗位id
+        vm.username = window.Role.username // 用户昵称
+        vm.rolePositionList = window.Role.positions // 岗位列表
+        console.log(JSON.stringify(vm.rolePositionList))
+        Vue.nextTick(function() {
+            vm.init()
+        })
         mui.os.plus && update()
         mui.os.plus && getVersion()
         var deceleration = mui.os.ios ? 0.003 : 0.0009;
@@ -1258,110 +1263,109 @@
             init();
             lf.event.fire(lf.window.currentWebview().opener(), 'dailyPaper', {})
         })
-
-        // 岗位切换
-        function switchRolePostion(val) {
-            var params = {
-                positionId: val
-            };
-            console.log(val)
-            lf.nativeUI.showWaiting();
-            lf.net.getJSON('user/switchPosition', params, function(data) {
-                console.log(JSON.stringify(data));
-                if(data.code == 200) {
-                    lf.nativeUI.closeWaiting();
-                    var obj = {
-                        usercode: data.data.id,
-                        username: data.data.name,
-                        phone: data.data.phone,
-                        companyId: data.data.companyId,
-                        userrole: data.data.positions[0].type,
-                        userroleName: data.data.positions[0].name,
-                        userroleId: data.data.positions[0].id,
-                        tonken: data.data.token,
-                        loginsign: '1',
-                        auths: data.data.auths,
-                        positions: data.data.userPositionList,
-                        currentPositions: data.data.positions,
-                        photograherId: data.data.photograherId
-                    }
-                    window.Role.save(obj)
-                    lf.nativeUI.toast('切换岗位成功');
-
-                    var windowCurrentPositionRoleId = window.Role.currentPositions[0].roleId;
-
-                    if(windowCurrentPositionRoleId == ROLE_EMUN.cityManager.id) {
-                        // 城市经理
-                        lf.window.openWindow(ROLE_EMUN.cityManager.windowId, '../' + ROLE_EMUN.cityManager.pageUrl,{},{});
-                    } else if (windowCurrentPositionRoleId == ROLE_EMUN.commissioner.id) {
-                        // 渠道
-                        lf.window.openWindow(ROLE_EMUN.commissioner.windowId, '../' + ROLE_EMUN.commissioner.pageUrl,{},{});
-                    } else if (windowCurrentPositionRoleId == ROLE_EMUN.officeManager.id) {
-                        //总经办
-                        // lf.window.openWindow(ROLE_EMUN.officeManager.windowId, '../' + ROLE_EMUN.officeManager.pageUrl,{},{});
-                    } else {
-                        lf.window.openWindow('order','../order/orderlist.html',{},{});
-                    }
-
-                    // if (window.Role.currentPositions[0].roleId!=12) {
-                    //     lf.window.openWindow('order','../order/orderlist.html',{},{},lf.window.currentWebview())
-                    // }
-                    init()
-                } else {
-                    lf.nativeUI.closeWaiting();
-                    lf.nativeUI.toast(data.msg);
-                }
-            }, function(erro) {
-                lf.nativeUI.closeWaiting();
-                lf.nativeUI.toast(erro.msg);
-            })
-        }
-        // 检测版本是否更新
-        function update() {
-            var params = {
-                "app_id": plus.runtime.appid,
-                "version": plus.runtime.version,
-                "imei": plus.device.imei,
-                "platform": plus.os.name
-            };
-            lf.net.getJSON("/app/validationversion", params, function(data) {
-                var update_desc = "发现新的版本，是否需要立即更新";
-                if(data.code == 200) {
-                    var btns = null;
-                    console.log(data.data.releaseUrl)
-                    if(data.data.isMandatory == 1) {
-                        update_desc = "发现新的版本，请立即更新";
-                        btns = ["立即更新"];
-                    } else {
-                        btns = ["立即更新", "取　　消"];
-                    }
-                    if(data.data.upgrade_desc) {
-                        update_desc = update_desc + "\n" + data.data.releaseRemark;
-                    }
-                    lf.nativeUI.confirm("", update_desc, btns, function(e) {
-                        if(btns.length == 1) {
-                            if(0 == e.index) {
-                                plus.runtime.openURL(data.data.releaseUrl);
-                                lf.window.closeCurrentWebview();
-                            } else {
-                                plus.runtime.quit();
-                            }
-                        } else {
-                            if(0 == e.index) {
-                                plus.runtime.openURL(data.data.releaseUrl);
-                                lf.window.closeCurrentWebview();
-                            } else {}
-                        }
-                    });
-                }
-            }, function(res) {});
-        }
-        // 得到版本
-        function getVersion() {
-            plus.runtime.getProperty(plus.runtime.appid,function(inf){
-                vm.wgtVer = inf.version;
-                console.log("当前应用版本：" + vm.wgtVer);
-            });
-        }
     })
+    // 岗位切换
+    function switchRolePostion(val) {
+        var params = {
+            positionId: val
+        };
+        console.log(val)
+        lf.nativeUI.showWaiting();
+        lf.net.getJSON('user/switchPosition', params, function(data) {
+            console.log(JSON.stringify(data));
+            if(data.code == 200) {
+                lf.nativeUI.closeWaiting();
+                var obj = {
+                    usercode: data.data.id,
+                    username: data.data.name,
+                    phone: data.data.phone,
+                    companyId: data.data.companyId,
+                    userrole: data.data.positions[0].type,
+                    userroleName: data.data.positions[0].name,
+                    userroleId: data.data.positions[0].id,
+                    tonken: data.data.token,
+                    loginsign: '1',
+                    auths: data.data.auths,
+                    positions: data.data.userPositionList,
+                    currentPositions: data.data.positions,
+                    photograherId: data.data.photograherId
+                }
+                window.Role.save(obj)
+                lf.nativeUI.toast('切换岗位成功');
+
+                var windowCurrentPositionRoleId = window.Role.currentPositions[0].roleId;
+
+                if(windowCurrentPositionRoleId == ROLE_EMUN.cityManager.id) {
+                    // 城市经理
+                    lf.window.openWindow(ROLE_EMUN.cityManager.windowId, '../' + ROLE_EMUN.cityManager.pageUrl,{},{});
+                } else if (windowCurrentPositionRoleId == ROLE_EMUN.commissioner.id) {
+                    // 渠道
+                    lf.window.openWindow(ROLE_EMUN.commissioner.windowId, '../' + ROLE_EMUN.commissioner.pageUrl,{},{});
+                } else if (windowCurrentPositionRoleId == ROLE_EMUN.officeManager.id) {
+                    //总经办
+                    // lf.window.openWindow(ROLE_EMUN.officeManager.windowId, '../' + ROLE_EMUN.officeManager.pageUrl,{},{});
+                } else {
+                    lf.window.openWindow('order','../order/orderlist.html',{},{});
+                }
+
+                // if (window.Role.currentPositions[0].roleId!=12) {
+                //     lf.window.openWindow('order','../order/orderlist.html',{},{},lf.window.currentWebview())
+                // }
+                init()
+            } else {
+                lf.nativeUI.closeWaiting();
+                lf.nativeUI.toast(data.msg);
+            }
+        }, function(erro) {
+            lf.nativeUI.closeWaiting();
+            lf.nativeUI.toast(erro.msg);
+        })
+    }
+    // 检测版本是否更新
+    function update() {
+        var params = {
+            "app_id": plus.runtime.appid,
+            "version": plus.runtime.version,
+            "imei": plus.device.imei,
+            "platform": plus.os.name
+        };
+        lf.net.getJSON("/app/validationversion", params, function(data) {
+            var update_desc = "发现新的版本，是否需要立即更新";
+            if(data.code == 200) {
+                var btns = null;
+                console.log(data.data.releaseUrl)
+                if(data.data.isMandatory == 1) {
+                    update_desc = "发现新的版本，请立即更新";
+                    btns = ["立即更新"];
+                } else {
+                    btns = ["立即更新", "取　　消"];
+                }
+                if(data.data.upgrade_desc) {
+                    update_desc = update_desc + "\n" + data.data.releaseRemark;
+                }
+                lf.nativeUI.confirm("", update_desc, btns, function(e) {
+                    if(btns.length == 1) {
+                        if(0 == e.index) {
+                            plus.runtime.openURL(data.data.releaseUrl);
+                            lf.window.closeCurrentWebview();
+                        } else {
+                            plus.runtime.quit();
+                        }
+                    } else {
+                        if(0 == e.index) {
+                            plus.runtime.openURL(data.data.releaseUrl);
+                            lf.window.closeCurrentWebview();
+                        } else {}
+                    }
+                });
+            }
+        }, function(res) {});
+    }
+    // 得到版本
+    function getVersion() {
+        plus.runtime.getProperty(plus.runtime.appid,function(inf){
+            vm.wgtVer = inf.version;
+            console.log("当前应用版本：" + vm.wgtVer);
+        });
+    }
 })()
