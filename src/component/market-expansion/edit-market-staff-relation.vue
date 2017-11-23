e0e9b125b6269bf4fc9490e262464567660907f4
wuzhitian@fingercrm.cn <wuzhitian@fingercrm.cn>
2017-11-23 17:31:25 +0800

    temp

--- src/component/market-expansion/edit-market-staff-relation.vue
+++ src/component/market-expansion/edit-market-staff-relation.vue
@@ -1,29 +1,76 @@
 <template>
     <div class="mui-inner-wrap market-expansion-edit-market-staff-relation">
-        <header class="mui-bar mui-bar-nav">
+        <header class="mui-bar">
             <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left" id="back"></a>
-            <h1 class="mui-title">精品照片</h1>
+            <h1 class="mui-title">编辑渠道关系</h1>
+            <div class="save-btn">保存</div>
         </header>
         <div class="mui-content">
-            <excellent-photo-header-summary
-            :chart-option="chicePhotoChartOptions"
-            :chice-photo-counts="chicePhotoCounts"
-            ></excellent-photo-header-summary>
-            <div>
-                <div class="ranking-list-title">精品照片评审人</div>
-                <ranking-list :list-data="rankingListData"></ranking-list>
-            </div>
-            <large-chart :chart-option="boutiquePhotosPhotographerChartOption" v-if="photographerChartOptionShow" :height="photographerChartOptionHeight"></large-chart>
-            <large-chart :chart-option="boutiquePhotosScenicRateChartOption"></large-chart>
+            <form class="mui-input-group">
+                <div class="mui-input-row">
+                    <label class="">旅行社：</label>
+                    <input type="number" class="" readonly placeholder="请输入旅行社">
+                </div>
+                <div class="mui-input-row">
+                    <label class="">产品名称：</label>
+                    <input type="number" class="" readonly placeholder="请输入产品名称">
+                </div>
+                <div class="mui-input-row">
+                    <label class=""><span class="required">* </span>大区：</label>
+                    <input type="text" class="area-selector" readonly placeholder="请选择大区">
+                </div>
+                <div class="mui-input-row">
+                    <label class=""><span class="required">* </span>城市：</label>
+                    <input type="text" class="" placeholder="请选择城市">
+                </div>
+                <div class="mui-input-row">
+                    <label class=""><span class="required">* </span>渠道专员：</label>
+                    <input type="text" class="" placeholder="请选择渠道专员">
+                </div>
+                <div class="mui-input-row">
+                    <label class=""><span class="required">* </span>有效起始时间：</label>
+                    <input type="text" class="" placeholder="请设置有效起始时间">
+                </div>
+                <div class="mui-input-row">
+                    <label class=""><span class="required">* </span>有效结束时间：</label>
+                    <input type="text" class="" placeholder="请设置有效结束时间">
+                </div>
+            </form>
         </div>
     </div>
 </template>
 <script>
-    export default {
+export default {
 
-    }
+};
 </script>
 <style lang="scss">
-
+.market-expansion-edit-market-staff-relation {
+    .mui-bar {
+        position: relative;
+        .save-btn {
+            position: absolute;
+            padding:4px 10px;
+            border: 1px solid #3cb493;
+            top: 6px;
+            right: 5px;
+            border-radius: 3px;
+            color: #3cb493;
+            background-color: #fff;
+            &:active {
+                background-color: #3cb493;
+                color: #fff;
+            }
+        }
+    }
+    
+    font-size: 0.3rem;
+    .mui-input-row label {
+        width: 40%;
+        .required {
+            color: red;
+        }
+    }
+}
 </style>
 
